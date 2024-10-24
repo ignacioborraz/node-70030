39451ada7da6916cd8a6d81a2fe58ffb443a2db6
igna <ignacioborraz@hotmail.com>
2024-10-24 09:58:18 -0300

    fix

--- src/utils/args.util.js
+++ src/utils/args.util.js
@@ -2,7 +2,7 @@ import { Command } from "commander";
 
 const args = new Command();
 
-args.option("-p <port>", "puerto", 8080);
+args.option("-p <port>", "puerto", 8000);
 args.option("--mode <mode>", "modo", "prod");
 
 args.parse();
